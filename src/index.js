097d02086515a0f3bf6f08db4971ec4dbe57f1f0
Dagabad <pisidavid24@gmail.com>
2020-01-30 22:22:35 +0000

    Agrego Id al modelo de datos, la tabla se muestra.

--- src/index.js
+++ src/index.js
@@ -3,29 +3,58 @@ import ReactDOM from "react-dom";
 import SubscriberData from "./data/subscribers.json";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      subscriberList: this.fillSubscriberListFromJson()
+      subscriberList: this.fillSubscriberListFromJson(),
+
+      // Otra forma de llamar a lista
+      // subscriberList: SubscriberData.map( (subscriber) => subscriber),
     };
   }
 
   fillSubscriberListFromJson() {
-    let subscriberList = SubscriberData.map(subscriber => {
-      return { subscriber };
+    let subscriberList = SubscriberData.map((subscriber) => {
+      return subscriber;
     });
 
     return subscriberList;
   }
 
   render() {
+
+    // Se puede usar para definirlo local, sin necesidad de escribir this.state
+    // const {subscriberList} = this.state;
+
     return (
+
       <div>
-        {/* {SubscriberData.map((subscriber, index)=>{
-            return <h1>{subscriber.name}</h1>
-            })} */}
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Last Name</th>
+              <th>Age</th>
+              <th>Sport</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.subscriberList.map( (subscriber) => {
+                return(
+                  <tr key={subscriber.id}>
+                    <td>{subscriber.name}</td>
+                    <td>{subscriber.lastName}</td>
+                    <td>{subscriber.age}</td>
+                    <td>{subscriber.sport}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
       </div>
-    );
+    )
   }
 }
 
